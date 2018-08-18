2506c34a3672268a8331ae7f95cf99b9b7b003b6
laowang <laowang@qq.com>
2018-08-18 16:31:27 +0800

    zhangsan

--- demo01.py
+++ demo01.py
@@ -1,17 +1,1 @@
-print("hello github")
-
-print("你好吗？")
-
-print("你快乐吗？")
-
-print("我很快乐!")
-
-print("下午好!")
-
-print("我用Python")
-
-print("人生苦短")
-
-#---------The End------------# 
-
-# tag
+print('zheshizhangsan')
